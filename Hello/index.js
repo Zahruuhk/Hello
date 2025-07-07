2d256af83bf0c8fb4eed149aa49b10aa180c9228
Zaruq Abdulmuqsit <olaniyizahruuhk@gmail.com>
2025-07-07 13:33:21 +0100

    fix: remove embedded .git and re-add Hello folder cleanly

---
+++ Hello/index.js
@@ -0,0 +1,1 @@
+console.log('Hello, Zaruq taking coding serious');
---
+++ basic.js
@@ -0,0 +1,19 @@
+//Variables
+let name = 'Zaruq';
+const age = 22;
+
+console.log('Name', name);
+console.log('Age', age);
+
+//loops
+
+for (let i = 1 ; i<= 5; i++ ) {
+    console.log('counting:',i);
+}
+
+//Functions
+function greet(user) {
+  return `Hello, ${user}!`;
+}
+
+console.log(greet(name));
