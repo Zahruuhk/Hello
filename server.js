657adc6baefab2fff5220aa269d01cbd98b4b11c
Zaruq Abdulmuqsit <olaniyizahruuhk@gmail.com>
2025-07-10 15:42:46 +0100

    json api and route , lets go man

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ app.get('/', (req, res) => {
     res.send('Hello, Express building');
 });
 
+app.get('/about', (req, res) => {
+  res.send('API JHOOR');
+});
+app.get('/api/user', (req, res) => {
+    res.json({
+       name: 'Zaruq Abdulmuqsit' ,
+       age: 22,
+       backendDev: true
+    });
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log('Server is listening on http://localhost:${PORT}');
+    console.log(`Server is listening on http://localhost:${PORT}`);
 });
